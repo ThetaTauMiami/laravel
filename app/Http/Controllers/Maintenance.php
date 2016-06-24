20a3d1c22f1d1bef75e786ea3da5ceea402161f2
Matt DePero <deperomm@miamioh.edu>
2016-06-24 16:10:56 -0400

    fixing route bug

--- app/Http/Controllers/Maintenance.php
+++ app/Http/Controllers/Maintenance.php
@@ -6,12 +6,34 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+
 class Maintenance extends Controller
 {
 
     public function deploy(){
 
-        return view("maintenance.deploy");
+        $process = new Process('ls -lsa');
+        $process->setTimeout(30);
+        $process->run();
+
+        $process2 = new Process('cd ../');
+        $process2->setTimeout(30);
+        $process2->run();
+
+        $process3 = new Process('ls');
+        $process3->setTimeout(30);
+        $process3->run();
+
+        // executes after the command finishes
+        if (!$process->isSuccessful()) {
+            throw new ProcessFailedException($process);
+        }
+
+        echo $process->getOutput();
+        echo $process2->getOutput();
+        echo $process3->getOutput();
 
     }
 }
