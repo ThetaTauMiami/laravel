b5c51e230fe955ea2d3a453546c869c16dfa5fce
Sam Mallamaci <sam.mallamaci@gmail.com>
2016-06-27 21:46:55 -0400

    added migrations for user, committee, and image

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,14 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
+            $table->increments('id')->primary();
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('ChapterClass');
+            $table->string('SchoolClass');
+            $table->boolean('ActiveStatus');
+            $table->integer('RankNumber');
             $table->rememberToken();
             $table->timestamps();
         });
