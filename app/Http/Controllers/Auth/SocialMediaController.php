026266cc74f96f24975d5f9970556b2bfa3ed936
Matt DePero <deperomm@miamioh.edu>
2016-07-12 22:17:23 -0400

    linkedin typos

--- app/Http/Controllers/Auth/SocialMediaController.php
+++ app/Http/Controllers/Auth/SocialMediaController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
-
+use App\Http\Controllers\Controller;
 use App\Http\Requests;
 
 class SocialMediaController extends Controller
