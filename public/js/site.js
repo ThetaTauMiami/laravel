c58311c8b5a5ff5b27060709e17817bcf9397a72
Matt DePero <deperomm@miamioh.edu>
2016-07-17 18:38:08 -0400

    type on animated scroll commit

---
+++ public/js/site.js
@@ -0,0 +1,20 @@
+
+
+// animate scrolling whenever an anchor link is clicked
+$(function() {
+	
+  var scrollTime = 1000; // Speed of animated scroll, lower = faster
+
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, scrollTime);
+        return false;
+      }
+    }
+  });
+});
