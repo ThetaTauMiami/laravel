44b437b5202258c9c0ab1ea5ce53afdd67907eb0
Thomas Burlett <burlette@miamioh.edu>
2016-07-19 21:41:00 -0400

    Front end changes
    Update a lot of designs for just about every page. Added a ton of
    placeholder content.

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -4,12 +4,49 @@
 	<div class="jumbotron" style="background-image:url('{{ asset('img/banner.jpg') }}'); background-position: center;">
         <h1>CONTACT US</h1>
     </div>
-    	<div class="row">
-		<div class="col-xs-12">
-			<h1>Content Goes Here</h1>
-			<p>
-				...
-			</p>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-offset-3">
+				CONTACT INFO HERE
+				<div class="well well-sm" style="margin-top: 30px">
+					<form class="form-horizontal" action="" method="post">
+						<fieldset>
+
+							<legend>Email Us</legend>
+							<!-- Name input-->
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="name">Name</label>
+								<div class="col-md-9">
+									<input id="name" name="name" type="text" placeholder="Your name" class="form-control">
+								</div>
+							</div>
+
+							<!-- Email input-->
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="email">Your E-mail</label>
+								<div class="col-md-9">
+									<input id="email" name="email" type="text" placeholder="Your email" class="form-control">
+								</div>
+							</div>
+
+							<!-- Message body -->
+							<div class="form-group">
+								<label class="col-md-3 control-label" for="message">Your message</label>
+								<div class="col-md-9">
+									<textarea class="form-control" id="message" name="message" placeholder="Please enter your message here..." rows="5"></textarea>
+								</div>
+							</div>
+
+							<!-- Form actions -->
+							<div class="form-group">
+								<div class="col-md-12 text-right">
+									<button type="submit" class="btn btn-danger btn-lg">Submit</button>
+								</div>
+							</div>
+						</fieldset>
+					</form>
+				</div>
+			</div>
 		</div>
 	</div>
 @stop
--- resources/views/pages/recruitment.blade.php
+++ resources/views/pages/recruitment.blade.php
@@ -2,15 +2,99 @@
 
 @section('content')
 	<div class="jumbotron" style="background-image:url('{{ asset('img/banner.jpg') }}'); background-position: center;">
-			<h1>RECRUITMENT INFO</h1>
-
+			<h1>CURRENT RUSH INFO</h1>
     </div>
    	<div class="row">
-		<div class="col-xs-12">
-			<h1>Content Goes Here</h1>
-			<p>
-				...
-			</p>
+		<div class="col-xs-12 text-center">
+			<h1>Upload current rush banner with info including dates and locations</h1>
 		</div>
 	</div>
+
+    <div class="container-fluid text-center">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p><p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p><p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+
+
+    </div>
+    <br>
+	<div class="jumbotron" style="background-image:url('{{ asset('img/banner.jpg') }}'); background-position: center;">
+		<h1>FAQ</h1>
+	</div>
+
+    <div class="container-fluid text-center">
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p><p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p><p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac mollis metus. Sed bibendum sagittis egestas. Nunc vel feugiat nibh. Vestibulum vehicula iaculis lorem, eu sodales nisl porta nec. Vivamus in orci nec ligula mattis egestas a vitae ipsum. Fusce fringilla libero eget ornare venenatis. Donec volutpat nisi nec diam feugiat, faucibus commodo urna efficitur.
+        </p>
+
+
+    </div>
 @stop
