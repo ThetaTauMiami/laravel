01e50e75dd404e3511b0ea2c1615d431b64b3c17
Thomas Burlett <burlette@miamioh.edu>
2016-08-25 16:52:36 -0400

    Updated contact page, removed useless jquery, fixed nav bar??????

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -1,20 +1,4 @@
-<script crossorigin="anonymous" integrity=
-"sha256-JmvOoLtYsmqlsWxa7mDSLMwa6dZ9rrIdtrrVYRnDRH0=" src=
-        "{{asset('https://code.jquery.com/jquery-3.0.0.min.js')}}">
-</script>
-
-
-<script crossorigin="anonymous" integrity=
-    "sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
-            src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js">
-    </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-
-
-<script src=
-        "{{asset('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js')}}">
-</script>
+<script   src="https://code.jquery.com/jquery-2.2.4.js"   integrity="sha256-iT6Q9iMJYuQiMWNd9lDyBUStIq/8PuOW33aOqmvFpqI="   crossorigin="anonymous"></script>
 
 <script src="{{asset('/js/jquery.inputmask.bundle.min.js')}}"></script>
 
