07bea17376bcc6cfc2a3216c0433110578daa47b
Matt DePero <deperomm@miamioh.edu>
2016-08-26 21:15:08 -0400

    finished bid process

--- app/Bid.php
+++ app/Bid.php
@@ -7,4 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Bid extends Model
 {
     protected $primaryKey = 'token';
+
+    public $timestamps = false;
 }
--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 
+use App\Bid;
+use DB;
+use Mail;
+
 class AdminController extends Controller
 {
 
@@ -20,16 +24,42 @@ class AdminController extends Controller
           'chapter_class' => 'required'
         ]);
 
-    	// foreach($request->roll_number as $key => $val){
-    	// 	$this->validate($request, [
-	    //       'roll_number.'.$key => 'required',
-	    //       'chapter_class.'.$key => 'required',
-	    //       'school_class.'.$key => 'required'
-	    //     ]);
-    	// }
+    	foreach( $request->roll_number as $key => $val){
+    		$token = $this->generateRandomString(128);
+
+    		$bid = new Bid;
+    		$bid->email = $request->email[$key];
+    		$bid->roll_number = $request->roll_number[$key];
+    		$bid->school_class = $request->school_class[$key];
+    		$bid->chapter_class = $request->chapter_class;
+    		$bid->token = $token;
+    		$bid->created_at = date("Y-m-d h:i:s");
+
+    		$bid->save();
+
+    		Mail::send('emails.registration', ["token"=>$token,"class"=>$request->chapter_class], function ($message) use ($request,$key) {
+			    $message->from('exec@thetataumiami.com', 'Theta Tau Miami');
+
+			    $message->to($request->email[$key]);
+			});
+    	}
 
 
-    	return "It worked!";
+
+    	return \Redirect::to('/admin');
 
     }
+
+
+
+
+    function generateRandomString($length = 10) {
+	    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+	    $charactersLength = strlen($characters);
+	    $randomString = '';
+	    for ($i = 0; $i < $length; $i++) {
+	        $randomString .= $characters[rand(0, $charactersLength - 1)];
+	    }
+	    return $randomString;
+	}
 }
