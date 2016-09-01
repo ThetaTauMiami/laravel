c5b09aab0d3e6d5e879eee5c18b8b889dc38969e
Thomas Burlett <burlette@miamioh.edu>
2016-08-31 21:15:44 -0400

    Updated recruitment page with current info
    current dates and stuff

--- resources/views/pages/recruitment.blade.php
+++ resources/views/pages/recruitment.blade.php
@@ -5,6 +5,7 @@
         <h1>CURRENT RUSH INFO</h1>
     </div>
     <br>
+    <!--
     <div class="container">
         <div class="row">
             <div class="col-xs-12 text-center">
@@ -27,15 +28,158 @@
     </div>
 
 		@if($complete == 1)
-			<script>
-				$(document).ready(function(){
-					bootbox.alert("Thanks for Signing up!");
-				});
+        <script>
+            $(document).ready(function(){
+                bootbox.alert("Thanks for Signing up!");
+            });
 
-			</script>
-		@endif
+        </script>
+    @endif
 
+            </div>
+        -->
+
+    <div class="container">
+        <div class="row">
+            <h1 style="color: #5B0000; text-align: center">Ready to Join Our Brotherhood?</h1>
+            <hr>
+            <div class="col-sm-offset-1 col-sm-5">
+                <div>
+                    <img class="img-responsive center-block" src="{{ asset('/img/fraternity-full.jpg') }}">
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <p style="font-size: medium">Theta Tau conducts recruitment at the beginning of each semester. Our
+                    recruitment process consists of
+                    events that allow our chapter to evaluate candidates on their values, credentials, and overall fit
+                    in our brotherhood. These events include Information Sessions, Informal Nights, and Formal
+                    Interviews.</p>
+                <br>
+                <p style="font-size: medium">For more information, read all of the information below and click this sign up button!</p>
+                <a href="/recruitment/signup">
+                    <button class="btn btn-warning" type="button">Sign up for Our Email List!</button>
+                </a>
+                @if($complete == 1)
+                    <script>
+                        $(document).ready(function(){
+                            bootbox.alert("Thanks for Signing up!");
+                        });
+
+                    </script>
+                @endif
+            </div>
+        </div>
     </div>
 
+    <div class="container">
+        <div class="row">
+            <h1 style="color: #5B0000; text-align: center">The Recruitment Process</h1>
+            <hr>
+            <div class="col-sm-offset-1 col-sm-5">
+                <div>
+                    <img style="height: 350px" class="img-responsive center-block"
+                         src="{{ asset('/img/icecream.jpg') }}">
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <h2>Open Events</h2>
+                <hr>
+                <p style="font-size: medium">Theta Tau conducts recruitment at the beginning of each semester. Our
+                    recruitment process consists of
+                    events that allow our chapter to evaluate candidates on their values, credentials, and overall fit
+                    in our brotherhood. These events include Information Sessions, Informal Nights, and Formal
+                    Interviews.</p>
+                <div class="well well-sm">
+                    <p><b style="color: #5B0000">CEC Trivia Night:</b> 9/1 @ 7pm EGB 270</p>
+                    <br>
+                    <p><b style="color: #5B0000">Public Speaking Workshop:</b> 9/8 6pm EGB 270</p>
+                    <br>
+                    <p><b style="color: #5B0000">Kickball & Chipotle:</b> 9/13 @ 4pm Millet Front Lawn</p>
+
+                </div>
+            </div>
+        </div>
+        <hr>
+        <br>
+        <div class="row">
+            <div class="col-sm-offset-1 col-sm-5">
+                <div>
+                    <img class="img-responsive center-block" src="{{ asset('/img/desk.jpg') }}">
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <h2>Information Sessions</h2>
+                <hr>
+                <p style="font-size: medium">Information nights allow interested students an opportunity to learn more
+                    about our chapter of Theta Tau and the recruitment process, ask questions, formally sign up for
+                    recruitment, and meet our brothers. Each semester we offer two information nights, but attendance is
+                    only expected and required at one of these. These sessions should last approximately 45 minutes and
+                    are casual dress.</p>
+                <div class="well well-sm">
+                    <p><b style="color: #5B0000">Session 1:</b> 9/20 @ 7:30pm KRG 319</p>
+                    <br>
+                    <p><b style="color: #5B0000">Session 2:</b> 9/21 @ 7:30pm KRG 319</p>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <br>
+        <div class="row">
+            <div class="col-sm-offset-1 col-sm-5">
+                <div>
+                    <img class="img-responsive center-block" src="{{ asset('/img/informal.jpg') }}">
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <h2>Informal Nights</h2>
+                <hr>
+                <p style="font-size: medium">The week following information sessions we host 3 “Informal Nights” in
+                    which students and brothers are given the opportunity to meet in small groups through “speed dating”
+                    sessions. This allows students the opportunity to have personal conversations with active brothers
+                    and learn more about what makes our members great. Attendance is strongly recommended at all three
+                    nights in order to get to know the maximum number of active brothers. These sessions should last two
+                    hours and are casual dress.</p>
+                <div class="well well-sm">
+                    <p><b style="color: #5B0000">Informal Night 1:</b> 9/27 7:30-9:30pm GAR 153</p>
+                    <br>
+                    <p><b style="color: #5B0000">Informal Night 2:</b> 9/28 7:30-9:30pm GAR 153</p>
+                    <br>
+                    <p><b style="color: #5B0000">Informal Night 3:</b> 9/29 7:30-9:30pm GAR 153</p>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <br>
+        <div class="row">
+            <div class="col-sm-offset-1 col-sm-5">
+                <div>
+                    <img class="img-responsive center-block" src="{{ asset('/img/exec.jpg') }}">
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <h2>Formal Interviews</h2>
+                <hr>
+                <p style="font-size: medium">Following informal nights, students that display a good fit for our chapter
+                    will be invited to participate in 2 of 3 nights of formal interviews during which they will discuss
+                    their professional experiences, achievements, goals, and their values in more depth with active
+                    brothers. Students will be asked to bring several copies of their resume and professional dress is
+                    strongly recommended for these events.
+                </p>
+                <div class="well well-sm">
+                    <p><b style="color: #5B0000">By Invitation Only</b></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <br>
+
+    <div class="container" style="text-align: center;">
+        <div class="row">
+            <h1 style="color: #5B0000; text-align: center">Contact Us</h1>
+            <hr>
+            <p style="font-size: medium">Feel free to reach out with any questions or concerns at <a href="mailto:thetataurecruitmentmu@gmail.com">thetataurecruitmentmu@gmail.com</a> </p>
+        </div>
+    </div>
 
 @stop
