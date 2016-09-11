b633136cec3ddb3e6594cdd57c80f188a068f6c9
Sam Mallamaci <sam.mallamaci@gmail.com>
2016-09-11 15:24:26 -0400

    uploading profile pictures is functional

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -10,6 +10,7 @@ use App\User;
 use Auth;
 use App\Image;
 use DB;
+use Illuminate\Support\Facades\Redirect;
 use Intervention\Image\ImageManagerStatic as Imager;
 
 class ProfileController extends Controller
@@ -41,45 +42,48 @@ class ProfileController extends Controller
       return view('pages.editProfile', compact('user', 'image'));
     }
 
+    //updates user values, and if an image has been uploaded, stores it and creates a thumbnail
     function update(Request $request, User $user){
       $user->update($request->all());
 
       //is there a value in the image section?
       if($request->image){
-        //$thumbnail = new Image;
-        //return var_dump($request->image);
-        $img = $request->file('image');
 
+        //$img is the newly uploaded file
+        $img = $request->file('image');
         $extension = $img->getClientOriginalExtension();
         $fileName = $img->getClientOriginalName();
         $publicPath = public_path();
         $filePath = "uploads/Profile_Full/{$fileName}";
         $request['filepath'] = $filePath;
+        $request['image'] = $img;
 
+        //validates that the new filepath and image are unique I guess, ask Cole.
         $this->validate($request, [
-            'filepath' => 'unique:images,file_path'
+            'filepath' => 'unique:images,file_path',
+            'image' => 'image'
         ]);
 
+        //sticks the new image into the folder
         $img->move("uploads/Profile_Full", $fileName);
-        //$img = Imager::make($filePath)->resize(200, 200)->save($filePath);
 
         //Stick that there image into this here database
         $image = new Image;
         $image->file_path = $filePath;
         $image->user_id = $user->id;
-        //$image->thumb_path = $filePath;
 
-        //Now make a neat thumbnail for the image
+        //creates the thumbnail image and associates it with the uploaded image
+        $image->thumb_path = $this->createThumbnail($filePath, $extension);
 
-        createThumbnail()
         $image->save();
 
         //and now make the user point to the new image
         $user->image_id = $image->id;
-      }
+      }//end image saving/replacing
 
-      return back();
+      return redirect('members/'.$user->id);
     }
+
     public function createThumbnail($image, $extension)
     {
       $withoutExt = preg_replace('/\\.[^.\\s]{3,4}$/', '', $image);
