3b0b5ccf3c826140269a2e47045ba98cd60b4597
Matt DePero <deperomm@miamioh.edu>
2016-09-12 08:43:29 -0400

    work with emails

--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -6,7 +6,7 @@
 </head>
 <body style="background-color: #5B0000; padding-left:10px;padding-right:10px;text-align:center;">
 
-	<div style="width:400px;max-width:100%;display:inline-block;background-color:#FFFFFF">
+	<div style="width:420px;max-width:100%;padding:8px;display:inline-block;background-color:#FFFFFF">
 
 		<div style="text-align:center;">
 			<img src="{{ $message->embed('img/login-logo.png') }}" style="width: 150px;max-width:100%;">
