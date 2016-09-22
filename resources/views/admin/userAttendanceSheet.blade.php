c417a658c9ff499756ce9881727e1a04bc4b5599
Cole Hoffbauer <colehoff7@gmail.com>
2016-09-21 23:29:35 -0400

    Made improvements to individual attendance sheet

--- resources/views/admin/userAttendanceSheet.blade.php
+++ resources/views/admin/userAttendanceSheet.blade.php
@@ -8,6 +8,44 @@
     <div class="container">
     	<div class="row">
     		<div class="col-xs-12">
+          <?php
+          $member = Auth::User();
+          $userAtt = DB::table('attendance')->where('user_id', '=', $member->id)->get();
+          $general = 0;
+          $service = 0;
+          $pd = 0;
+          $brotherhood = 0;
+          foreach($userAtt as $ua){
+            $event = DB::table('events')->where('id', '=', $ua->event_id)->where('semester_id', '=', app('App\Http\Controllers\HomeController')->getCurrentSemester()->id)->first();
+
+            if($event->type_id == "General"){
+              $general += $event->points;
+            }elseif($event->type_id == "Service"){
+              $service += $event->points;
+            }elseif($event->type_id == "PD"){
+              $pd += $event->points;
+            }elseif($event->type_id == "Brotherhood"){
+              $brotherhood += $event->points;
+            }
+
+          }
+           ?>
+         <table class="table">
+           <thead>
+             <tr>
+               <th>General Points</th>
+               <th>Service Points</th>
+               <th>PD Points</th>
+               <th>Brotherhood Points</th>
+             </tr>
+           </thead>
+         <tr>
+           <td>{{$general}}</td>
+           <td>{{$service}}</td>
+           <td>{{$pd}}</td>
+           <td>{{$brotherhood}}</td>
+         </tr>
+       </table>
           <div class="table-responsive">
           <table class="table">
             <thead>
