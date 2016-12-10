3058c1e25742722adaf556a97e7ce8af9c8f931e
Cole Hoffbauer <colehoff7@gmail.com>
2016-12-10 15:10:54 -0500

    Now things are being organize alphabetically. the last one was a joke. ha.

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -80,7 +80,7 @@ class EventsController extends Controller
         $attended = DB::table('users')
         ->whereIn('id', $ids)
         ->where('active_status', '=', 1)
-        ->orderBy('roll_number')
+        ->orderBy('first_name')
         ->get();
       }
       return view('events.takeAttendanceVariable', compact('event', 'attended', 'attendance'));
