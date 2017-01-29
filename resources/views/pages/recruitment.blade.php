3dc9dd3e30e3f2e6fafc4661d6fc4ad7ae161537
Matt DePero <deperomm@miamioh.edu>
2017-01-29 15:11:17 -0500

    added button to 'add another' feature to recruitment email sign up

--- resources/views/pages/recruitment.blade.php
+++ resources/views/pages/recruitment.blade.php
@@ -64,7 +64,7 @@
                 @if($complete == 1)
                     <script>
                         $(document).ready(function(){
-                            bootbox.alert("Thanks for Signing up!");
+                            bootbox.alert("Thanks for Signing up!<br/><a class='btn btn-secondary' href='/recruitment/signup'>Add Another</a>");
                         });
 
                     </script>
