c4fd8d80e29affc6508e91f03afbdd202bfd9a41
mdepero <deperomm@miamioh.edu>
2017-09-07 08:41:42 -0400

    resume path bug, path must be to public folder

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -50,7 +50,7 @@ class HomeController extends Controller
     }
 
     function resume(User $user) {
-      return Response::make(file_get_contents(public_path().$user->resume_path), 200, [
+      return Response::make(file_get_contents(public_path().'/'.$user->resume_path), 200, [
           'Content-Type' => 'application/pdf',
           'Content-Disposition' => 'inline; filename="'.$user->first_name.' '.$user->last_name.' [Miami University - Theta Tau] Resume.pdf"'
       ]);
