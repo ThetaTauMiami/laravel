61a488a06568aeb90b3516d1d9130200117d25a7
Cole Hoffbauer <colehoff7@gmail.com>
2018-02-19 20:06:34 -0500

    fixed database seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
 
         DB::table('users')->insert([
-            'first_name'=>'John',
-            'last_name'=>'Doe',
+            'first_name'=>'Admin',
+            'last_name'=>'Testerson',
             'email'=>'admin@admin.com',
             'phone'=>'5135551234',
             'password'=>bcrypt('Password1'),
@@ -22,6 +22,24 @@ class DatabaseSeeder extends Seeder
             'roll_number'=>1,
             'school_class'=>2010,
             'active_status'=>1
+        ]);
+
+        DB::table('roles')->insert([
+            'name' => 'Admin',
+            'type' => 'admin',
+            'rank_order' => 1,
+            'active' => 1
+        ]);
+
+        DB::table('semesters')->insert([
+            'name' => 'Spring 2018',
+            'date_start' => 2018-01-01
+        ]);
+
+        DB::table('role_user')->insert([
+            'user_id' => 1,
+            'role_id' => 1,
+            'semester_id' => 1
         ]);
     }
 }
